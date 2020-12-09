c749f27fb635f1bfbcd86c5680e3458b3f638bd2
Jason Herrera <baytownjason@yahoo.com>
2020-12-09 22:38:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
   sum = num1.to_i + num2.to_i
+  sum
 end
 
 def subtraction(num1, num2)
   difference = num1.to_i - num2.to_i
+  difference
 end
 
 def division(num1, num2)
   quotient = num1.to_i / num2.to_i
+  quotient
 end
 
 def multiplication(num1, num2)
   product = num1.to_i * num2.to_i
+  product
 end
 
 def modulo(num1, num2)
   model = num1.to_i % num2.to_i
+  model
 end
 
 def square_root(num)
   sq = num.to_i **
+  sq
 end
